52d90f8c8400beb586e7fcfb7aa768830ee926a8
Maaruf Dauda <maarufdauda7@gmail.com>
2018-06-28 18:18:55 +0100

    splash screen added

--- app/src/main/java/com/alc/askemkay/journalapp/LoginActivity.java
+++ app/src/main/java/com/alc/askemkay/journalapp/LoginActivity.java
@@ -3,11 +3,11 @@ package com.alc.askemkay.journalapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_login);
     }
 }
